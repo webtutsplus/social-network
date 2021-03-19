8d21d4cf9070e0911eac9c5d012db178fc496114
nmadhab <nilmadhab1994@booking.com>
2021-03-19 13:07:01 +0530

    Post mapping not working due to CORS

--- springbackend/src/main/java/com/simplecoding/social/controller/PrivateEndpoint.java
+++ springbackend/src/main/java/com/simplecoding/social/controller/PrivateEndpoint.java
@@ -6,25 +6,23 @@ import com.simplecoding.social.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("private")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PrivateEndpoint {
 
     @Autowired
     UserService userService;
 
-    @GetMapping("/user-details")
+    @GetMapping("user-details")
     public ResponseEntity<?> getUserInfo(@AuthenticationPrincipal UserDto userDto) {
         User user = userService.getUser(userDto.getEmail());
         return ResponseEntity.ok(user);
     }
 
-    @PostMapping("/saveUser")
+    @GetMapping("saveUser")
     public ResponseEntity<UserDto> saveUserInfo(@AuthenticationPrincipal UserDto userDto) {
         userService.saveUser(userDto);
         return ResponseEntity.ok(userDto);
