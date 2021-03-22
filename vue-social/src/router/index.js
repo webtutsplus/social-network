183717c59ed81a10fbb149ed63348ac3701b8821
Shivam Agarwal <shivam.agrawal.2000@gmail.com>
2021-03-22 20:53:16 +0530

    Added Chat Functionality

--- vue-social/src/router/index.js
+++ vue-social/src/router/index.js
@@ -4,6 +4,9 @@ import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import SignUp from '../views/SignUp.vue'
 import UserList from '../views/ListUsers.vue'
+import Chat from '../components/Chat.vue'
+import Room from '../components/Room.vue'
+import AddRoom from '../components/AddRoom.vue'
 
 Vue.use(VueRouter)
 
@@ -26,11 +29,27 @@ const routes = [
     path: '/listusers',
     name: 'ListUsers',
     component: UserList
+  },
+  {
+    path: '/chat/:nickname/:roomid/:roomname',
+    name: 'Chat',
+    component: Chat
+  },
+  {
+    path: '/room/:nickname',
+    name: 'RoomList',
+    component: Room
+  },
+  {
+    path: '/add-room',
+    name: 'AddRoom',
+    component: AddRoom
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
