3e0d57fb02337da8adda4e38660715fbf4e0ac26
tailorsmit <sjtailor27@gmail.com>
2021-03-22 17:41:40 +0530

    add friend feature.

--- vue-social/src/views/listuers.vue
+++ vue-social/src/views/listuers.vue
@@ -13,6 +13,9 @@
         <div class="col">
           <img class="img" v-bind:src="item.picture" alt="avatar">
         </div>
+        <div v-if="email !== item.email">
+          <button class="btn btn-primary pull-right" @click="addfriend(item.id)">Add Friend</button>
+        </div>
       </div>
     </li>
   </ul>
@@ -20,16 +23,20 @@
 
 <script>
 import axios from 'axios';
+import firebase from 'firebase';
 
 export default {
   name: "listuers",
   data() {
     return {
       users: [],
+      email: '',
     }
   },
-  mounted() {
+   mounted() {
     this.getusers();
+    //console.log(firebase.auth().currentUser.email)
+    this.email = firebase.auth().currentUser.email;
   },
   methods: {
     getusers() {
@@ -44,6 +51,16 @@ export default {
       }).catch(err => {
         console.log(err)
       })
+    },
+    addfriend(id){
+      axios.get('http://localhost:8080/private/addFriend/?friendId='+id,{'headers' :{
+          'Authorization': 'Bearer '+localStorage.getItem('idToken'),
+        }}).then(resp => {
+          console.log(resp.data);
+          if(resp.status === 200){
+            alert("friend added! ");
+          }
+      }).catch(err => console.log(err))
     }
   }
 }
