f8c4497a88a3af178cfba86770677abc4dc47a8b
Shivam Agarwal <shivam.agrawal.2000@gmail.com>
2021-03-26 14:04:13 +0530

    Added CORS

--- springbackend/src/main/java/com/simplecoding/social/SpringandfirebaseauthApplication.java
+++ springbackend/src/main/java/com/simplecoding/social/SpringandfirebaseauthApplication.java
@@ -14,4 +14,13 @@ public class SpringandfirebaseauthApplication {
     }
 
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
 }
