7f5a1ad5d4f6b9ff72ed6c6e70bd6d4ae8a5fa24
Smit Tailor <sjtailor27@gmail.com>
2021-03-27 13:22:28 +0530

    all post endpoints added

--- vue-social/src/views/Posts.vue
+++ vue-social/src/views/Posts.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <button class="nav-link" @click="getPost()">All posts</button>
+      </li>
+      <li class="nav-item">
+        <button class="nav-link" @click="getmypost()">My posts</button>
+      </li>
+      <li class="nav-item">
+        <select v-on:change="getpostofuser()" v-model="selected">
+          <option v-for="item in this.users" :key="item.id" :value="item.id">{{item.email}}</option>
+        </select>
+      </li>
+    </ul>
+    <label>Content : </label><input type="text" v-model="content"/>
+    <button @click="addpost()">Add Post</button>
     <div v-for="items in posts" :key="items.id" class="card" style="">
       <div class="card-body">
         <h5 class="card-title">{{ items.user.name }}</h5>
@@ -19,16 +34,46 @@ export default {
   data() {
     return {
       posts: [],
+      content: "",
+      users :[],
+      selected: null,
     }
   },
   mounted() {
     this.getPost();
+    this.getusers();
   },
   methods: {
     getPost() {
       axios.get(API_BASE_URL + "public/posts").then(resp => {
         this.posts = resp.data
       });
+    },
+    getmypost(){
+      axios.get(API_BASE_URL + "private/mypost",{"headers" : {
+          "Authorization":"Bearer "+localStorage.getItem("idToken")
+        }}).then(res => {
+          this.posts = res.data
+      }).catch(err => console.log(err));
+    },
+    addpost(){
+      axios.post(API_BASE_URL+"private/addpost",{"content" : this.content},{"headers" : {
+          "Authorization":"Bearer "+localStorage.getItem("idToken")
+        }}).then(res => {
+          alert("post added");
+          console.log(res.data);
+      }).catch(err => console.log(err));
+    },
+    getpostofuser(){
+      console.log(this.selected);
+      axios.get(API_BASE_URL + "public/"+this.selected+"/posts").then(res => {
+        this.posts = res.data
+      }).catch(err => console.log(err));
+    },
+    getusers() {
+      axios.get(API_BASE_URL+"public/users").then(res => {
+        this.users = res.data
+      }).catch(err => console.log(err));
     }
   }
 }
